efcb5aad10861b5ea7e380edc51a70becd7e1c45
EliteBook <kylehensman@gmail.com>
2018-11-12 14:34:54 -0400

    I removed old/lingering files and reference to old files within the code. I updated the package file with a new locally scoped gulp script to ensure gulp doesn't need to be installed globally. I updated gulp and gulp related packages and updated the gulpfile task/configuration to ensure this is working as expected. I updated the docker-compose.yml file to remove the data volume pointing to node_modules. I updated the dockerfile to ensure both the package.json and package-lock.json files are beeing added to the image/container. I ran the new local gulp command to ensure all files are updated and removed all lingering files that are not needed anymore.

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,61 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
 
 // Copy third party libraries from /node_modules into /vendor
 gulp.task('vendor', () => {
+
   // Bootstrap
   gulp.src([
-    'node_modules/bootstrap/dist/**/*',
-    '!node_modules/bootstrap/dist/css/bootstrap-grid*',
-    '!node_modules/bootstrap/dist/css/bootstrap-reboot*',
-  ])
-    .pipe(gulp.dest('public/vendor/bootstrap'));
+      './node_modules/bootstrap/dist/**/*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+    ])
+    .pipe(gulp.dest('./public/vendor/bootstrap'))
+
   // Font Awesome
   gulp.src([
-    'node_modules/font-awesome/**/*',
-    '!node_modules/font-awesome/{less,less/*}',
-    '!node_modules/font-awesome/{scss,scss/*}',
-    '!node_modules/font-awesome/.*',
-    '!node_modules/font-awesome/*.{txt,json,md}',
-  ])
-    .pipe(gulp.dest('public/vendor/font-awesome'));
+      './node_modules/@fortawesome/**/*',
+    ])
+    .pipe(gulp.dest('./public/vendor'))
+
 });
+
 // Compile SCSS
 gulp.task('css:compile', () => {
-  return gulp.src('public/scss/**/*.scss')
+  return gulp.src('./public/scss/**/*.scss')
     .pipe(sass.sync({
-      outputStyle: 'expanded',
+      outputStyle: 'expanded'
     }).on('error', sass.logError))
-    .pipe(gulp.dest('public/css'));
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./public/css'))
 });
+
 // Minify CSS
 gulp.task('css:minify', ['css:compile'], () => {
   return gulp.src([
-    'public/css/*.css',
-    '!public/css/*.min.css',
-  ])
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+      './public/css/*.css',
+      '!./public/css/*.min.css'
+    ])
+    .pipe(cleanCSS())
     .pipe(rename({
-      suffix: '.min',
+      suffix: '.min'
     }))
-    .pipe(gulp.dest('public/css'));
+    .pipe(gulp.dest('./public/css'))
 });
+
 // CSS
 gulp.task('css', ['css:compile', 'css:minify']);
-// Minify JavaScript
-gulp.task('js:minify', () => {
-  return gulp.src([
-    'public/js/*.js',
-    '!public/js/*.min.js',
-  ])
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min',
-    }))
-    .pipe(gulp.dest('public/js'));
-});
-// JS
-gulp.task('js', ['js:minify']);
 
 // Default task
-gulp.task('default', ['css', 'js', 'vendor']);
+gulp.task('default', ['css', 'vendor']);
+
 // Dev task
-gulp.task('dev', ['css', 'js'], () => {
-  gulp.watch('public/scss/**/*.scss', ['css']);
-  gulp.watch('public/js/*.js', ['js']);
+gulp.task('dev', ['css'], () => {
+  gulp.watch('./public/scss/*.scss', ['css']);
 });
