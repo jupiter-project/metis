ffd2b746389ea50f53c52e67700d424546b1ff0a
jonathanramirez <jonathan@stackitgroup.com>
2021-05-21 17:06:50 -0500

    remove solarwind config

--- utils/logger.js
+++ utils/logger.js
@@ -4,18 +4,8 @@ const {
   transports,
 } = require('winston');
 require('winston-mongodb');
-const os = require('os');
-require('winston-syslog');
 
 const { hasJsonStructure } = require('../utils/utils');
-
-const papertrail = new transports.Syslog({
-  host: process.env.PAPERTRAIL_HOST,
-  port: process.env.PAPERTRAIL_PORT,
-  protocol: 'tls4',
-  localhost: os.hostname(),
-  eol: '\n',
-});
 
 const mongoDbTransport = new transports.MongoDB({
   level: 'error',
@@ -38,13 +28,12 @@ const transportsArray = [
     level: 'error',
     maxsize: 5120000,
     maxFiles: 5,
-    filename: `${__dirname}/../logs/log-api.log`,
+    filename: '/var/log/metis/log-api.log',
     format: format.combine(format.timestamp(), format.json()),
   }),
   new transports.Console({
     level: 'info',
   }),
-  papertrail,
 ];
 
 if (process.env.NODE_ENV === 'production') {
