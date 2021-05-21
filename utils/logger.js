d63b465dfc4639ebead532ca0faf7bfe4d6bc805
jonathanramirez <jonathan@stackitgroup.com>
2021-05-21 14:04:14 -0500

    logger

--- utils/logger.js
+++ utils/logger.js
@@ -4,8 +4,25 @@ const {
   transports,
 } = require('winston');
 require('winston-mongodb');
+require('winston-papertrail').Papertrail;
 
 const { hasJsonStructure } = require('../utils/utils');
+
+const winstonPapertrail = new transports.Papertrail({
+  level: 'info',
+  host: process.env.PAPERTRAIL_HOST,
+  port: process.env.PAPERTRAIL_PORT,
+});
+
+const mongoDbTransport = new transports.MongoDB({
+  level: 'error',
+  db: process.env.URL_DB,
+  options: {
+    useUnifiedTopology: true,
+  },
+  collection: 'metis-logs',
+  format: format.combine(format.timestamp(), format.json()),
+});
 
 const getMessageFormat = (message) => {
   return hasJsonStructure(message)
@@ -19,24 +36,16 @@ const transportsArray = [
     maxsize: 5120000,
     maxFiles: 5,
     filename: `${__dirname}/../logs/log-api.log`,
+    format: format.combine(format.timestamp(), format.json()),
   }),
   new transports.Console({
     level: 'info',
   }),
+  winstonPapertrail,
 ];
 
 if (process.env.NODE_ENV === 'production') {
-  transportsArray.push(
-    new transports.MongoDB({
-      level: 'error',
-      db: process.env.URL_DB,
-      options: {
-        useUnifiedTopology: true,
-      },
-      collection: 'metis-logs',
-      format: format.combine(format.timestamp(), format.json()),
-    }),
-  );
+  transportsArray.push(mongoDbTransport);
 }
 
 module.exports = createLogger({
